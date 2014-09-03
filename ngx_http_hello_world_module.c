b6b75965e240d82aa574a201510e2de20a23f15f
erankor <eran.kornblau@kaltura.com>
2014-09-03 09:59:30 -0400

    reproduce stuck connection bug

--- ngx_http_hello_world_module.c
+++ ngx_http_hello_world_module.c
@@ -95,36 +95,58 @@ ngx_module_t ngx_http_hello_world_module = {
  * @return
  *   The status of the response generation.
  */
+#if 0
 static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t *r)
 {
     ngx_buf_t *b;
     ngx_chain_t out;
 
-    /* Set the Content-Type header. */
     r->headers_out.content_type.len = sizeof("text/plain") - 1;
     r->headers_out.content_type.data = (u_char *) "text/plain";
-
-    /* Allocate a new buffer for sending out the reply. */
-    b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
-
-    /* Insertion in the buffer chain. */
-    out.buf = b;
-    out.next = NULL; /* just one buffer */
-
-    b->pos = ngx_hello_world; /* first position in memory of the data */
-    b->last = ngx_hello_world + sizeof(ngx_hello_world); /* last position in memory of the data */
-    b->memory = 1; /* content is in read-only memory */
-    b->last_buf = 1; /* there will be no more buffers in the request */
-
-    /* Sending the headers for the reply. */
     r->headers_out.status = NGX_HTTP_OK; /* 200 status code */
-    /* Get the content length of the body. */
     r->headers_out.content_length_n = sizeof(ngx_hello_world);
-    ngx_http_send_header(r); /* Send the headers */
+    ngx_http_send_header(r);
 
+	
     /* Send the body, and return the status code of the output filter chain. */
-    return ngx_http_output_filter(r, &out);
-} /* ngx_http_hello_world_handler */
+    //return ngx_http_output_filter(r, &out);
+    return NGX_ERROR;
+}
+#endif
+
+static void
+event_callback(ngx_event_t *ev)
+{
+	ngx_http_request_t *r = (ngx_http_request_t *)ev->data;
+	
+	ngx_http_finalize_request(r, NGX_ERROR);
+}
+
+static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t *r)
+{
+    ngx_buf_t *b;
+    ngx_chain_t out;
+	ngx_event_t *complete_event;
+
+    r->headers_out.content_type.len = sizeof("text/plain") - 1;
+    r->headers_out.content_type.data = (u_char *) "text/plain";
+    r->headers_out.status = NGX_HTTP_OK; /* 200 status code */
+    r->headers_out.content_length_n = sizeof(ngx_hello_world);
+    ngx_http_send_header(r);
+	
+	complete_event = ngx_pcalloc(r->pool, sizeof(*complete_event));
+	complete_event->handler = event_callback;
+	complete_event->data = r;
+	complete_event->log = r->connection->log;
+	
+	ngx_add_timer(complete_event, 1000);
+	
+	r->main->count++;
+	
+	return NGX_DONE;
+}
+
+
 
 /**
  * Configuration setup function that installs the content handler.
